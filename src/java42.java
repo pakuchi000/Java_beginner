834215fc1d6adf9fac243f587575ad223c874d01
kanhiroki <kan_h@itc-tokyo.com>
2021-01-21 16:49:18 +0900

    第5回後

---
+++ src/java42.java
@@ -0,0 +1,22 @@
+
+public class java42 {
+
+	public static void main(String[] args) {
+
+		String i ="ABC";
+
+		switch(i) {
+		case "ABC":
+			System.out.println("ABC　だよ");
+			break;
+		case "DEF":
+			System.out.println("DEF　かも");
+			break;
+		default:
+			System.out.println("わかんない");
+			break;
+		}
+
+	}
+
+}
---
+++ src/java43.java
@@ -0,0 +1,21 @@
+
+public class java43 {
+
+	public static void main(String[] args) {
+
+		for(int i = 0; i < 5; i++) {
+			System.out.println(i + "回目のHello!");
+		}
+
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + "回目のHello!");
+		}
+
+		for(int i = 1; i <= 10; i++) {
+			System.out.println(i + "回目のHello!");
+		}
+
+
+	}
+
+}
