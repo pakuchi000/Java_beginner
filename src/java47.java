dc30f3e42535a3c87eec34b9a9672f0fd4a0dbf2
paaac <paku0215huu@gmail.com>
2021-01-21 22:10:14 +0900

    第6回後

---
+++ src/java47.java
@@ -0,0 +1,17 @@
+
+public class java47 {
+
+	public static void main(String[] args) {
+
+		for(int i =0; i<5; i++) {
+			for(int j =0; j<5; j++) {
+				System.out.print("■　");
+			}
+
+			System.out.println(i + "ループ目です。");
+
+		}
+
+	}
+
+}
